54d5b6293eeb7c01821eaeb914e49d00b56d872b
Heetesh.Panghanti <Heetesh.Panghanti@acentra.com>
2024-07-01 20:30:20 +0530

    Unique Pairs solution

---
+++ Forum8/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Text;
+
+namespace Forum8
+{
+    internal class Program
+    {
+        private static readonly IDictionary<int, int> pairs = new Dictionary<int, int>();
+
+        static int[] SortArray(int[] array)
+        {
+            int length = array.Length;
+            for (int iterator1 = 1; iterator1 < length; iterator1++)
+            { 
+                int element = array[iterator1];
+                int iterator2 = iterator1 - 1;
+
+                while (iterator2 >= 0 && array[iterator2] > element)
+                {
+                    array[iterator2+1] = array[iterator2];
+                    iterator2--;
+                }
+
+                array[iterator2+1] = element;
+            }
+
+            return array;
+        }
+
+        static void GetUniquePairs(int[] array, int target)
+        {
+            int length = array.Length;
+            for (int index = 0; index <= length; index++)
+            {
+                for (int jindex = index + 1; jindex < length; jindex++)
+                {
+                    if ((array[index] + array[jindex]) == target && !pairs.ContainsKey(array[index]))
+                    {
+                        pairs.Add(array[index], array[jindex]);
+                    }
+                }
+            }
+
+            OutputUniquePairs(pairs);
+        }
+
+        private static void OutputUniquePairs(IDictionary<int, int> pairs)
+        {
+            int tempCounter = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append('{');
+            int count = pairs.Count;
+            foreach (var pair in pairs)
+            {
+                stringBuilder.Append(string.Format($"({pair.Key},{pair.Value})"));
+                tempCounter++;
+
+                if (tempCounter != count)
+                {
+                    stringBuilder.Append(',');
+                }
+            }
+
+            stringBuilder.Append('}');
+            Console.WriteLine(stringBuilder);
+        }
+
+        static bool IsInputValid(int[] array, int target)
+        {
+            if (array == null || array.Length == 0) {
+                Console.WriteLine($"Array shall not be empty!");
+                return false;
+            }
+
+            if (target >= int.MaxValue)
+            {
+                Console.WriteLine($"Target value shall not exceed integer maximum value {int.MaxValue}");
+                return false;
+            }
+
+            if (array.Any(x => x >= int.MaxValue))
+            {
+                Console.WriteLine($"Array element value shall not exceed integer maximum value {int.MaxValue}!");
+                return false;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int target = 7;
+            int[] array =  { 2,2,4,3,4,0,7,3,5, 5};//Scenario where duplicate may occur if sort not applied//{ 2, 4, 3, 7, 8, 5, 9 };
+
+            #region Input from Console
+            Console.WriteLine("\nPlease enter the array size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            array = new int[size];
+            Console.WriteLine("\nPlease enter the array elements space separated");
+            array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+            Console.WriteLine("\nPlease enter the target value");
+            target = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region Validate Input
+            if (!IsInputValid(array, target)) {
+                Environment.Exit(0);
+            }
+            #endregion
+
+            #region Sort Array
+            array = SortArray(array);
+            #endregion Sort Array
+
+            GetUniquePairs(array, target);
+            Console.ReadLine();
+        }
+    }
+}
